f67d6f07bc1046ffef838d9b33b45738563469db
Vladislav Kostylev <kostylev.v@gmail.com>
2018-10-02 13:28:15 +0300

    Stage #2: Count needed resources

--- src/coffeeMachine/Main.java
+++ src/coffeeMachine/Main.java
@@ -1,7 +1,25 @@
 package coffeeMachine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write how many cups of coffee you will need:");
+        try {
+          int cups = scanner.nextInt();
+          System.out.printf("For %d cups of coffee you will need: %n", cups);
+          System.out.printf("%d ml of water %n", cups*WATER);
+          System.out.printf("%d ml of milk %n", cups*MILK);
+          System.out.printf("%d g of coffee beans %n", cups*BEANS);
+
+        } catch (Exception ioe) {
+          System.out.println("Cups should be an Integer value");
+        }
     }
+
+  private static final int WATER = 200;
+  private static final int MILK = 50;
+  private static final int BEANS = 15;
+
 }
