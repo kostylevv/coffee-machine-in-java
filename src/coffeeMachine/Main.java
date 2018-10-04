054d2f6430be70eec13d1681e9efe0b3cc14c258
Vladislav Kostylev <kostylev.v@gmail.com>
2018-10-04 09:00:45 +0300

    Stage #4: Choosing different actions

--- src/coffeeMachine/Main.java
+++ src/coffeeMachine/Main.java
@@ -1,114 +1,226 @@
 package coffeeMachine;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    private static final int WATER_CONSUMPTION = 200;
-    private static final int MILK_CONSUMPTION = 50;
-    private static final int BEANS_CONSUMPTION = 15;
 
     public static void main(String[] args) {
+      boolean cont = true;
+      VendingMachine m = new VendingMachine();
+      System.out.println(m.getState());
+      String line;
+      Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+      while (cont) {
+        System.out.print("Write action (buy, fill, take, exit):");
+          try {
+            line = scanner.nextLine().trim();
+              switch (line) {
+              case "buy":
+                  System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                  line = scanner.nextLine().trim();
+                  switch (line) {
+                    case "1":
+                      m.order(Drink.ESPRESSO);
+                      break;
 
-        try {
-          System.out.print("Write how many ml of water the coffee machine has: ");
-          int water = scanner.nextInt();
-          System.out.print("Write how many ml of milk the coffee machine has: ");
-          int milk = scanner.nextInt();
-          System.out.print("Write how many grams of coffee the coffee machine has: ");
-          int beans = scanner.nextInt();
-          System.out.print("Write how many cups of coffee you will need: ");
-          int cups = scanner.nextInt();
+                    case "2":
+                      m.order(Drink.LATTE);
+                      break;
 
-          CoffeeMachine machine = new CoffeeMachine.Builder(WATER_CONSUMPTION,
-          MILK_CONSUMPTION, BEANS_CONSUMPTION).water(water).beans(beans).milk(milk).build();
+                    case "3":
+                      m.order(Drink.CAPPUCHINO);
+                      break;
 
-          int capacity = machine.getBrewCapacity();
+                    default:
+                      System.out.println("Unsupported drink");
+                  }
+                System.out.print(m.getState());
+                System.out.println();
+                break;
 
-          if (capacity > cups) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that) %n", capacity-cups);
-          } else if (capacity == cups) {
-            System.out.println("Yes, I can make that amount of coffee");
-          } else {
-            System.out.printf("No, I can make only %d cups of coffee %n", capacity);
+              case "fill":
+                  System.out.println("Write how many ml of water do you want to add:");
+                  int water = Integer.parseInt(scanner.nextLine());
+                  System.out.println("Write how many ml of milk do you want to add:");
+                  int milk = Integer.parseInt(scanner.nextLine());
+                  System.out.println("Write how many grams of coffee beans do you want to add:");
+                  int beans = Integer.parseInt(scanner.nextLine());
+                  System.out.println("Write how many disposable cups do you want to add:");
+                  int cups = Integer.parseInt(scanner.nextLine());
+                  m.refill(Refill.getInstance(water,milk,beans,cups));
+                  System.out.print(m.getState());
+                  System.out.println();
+                  break;
+
+              case "take":
+                  int cash = m.take();
+                  System.out.println("I gave you $" + cash);
+                  System.out.print(m.getState());
+                  System.out.println();
+                  break;
+
+              case "exit":
+                  cont = false;
+                  break;
+
+              default:
+                  //System.out.println("Unsupported command");
+            } //eof switch
+          } catch (Exception e) {
+            System.out.println(e.getMessage());
           }
-          
-        } catch (Exception ioe) {
-          System.out.println("Please enter an an Integer value");
-        }
-    }
+      }//eof while
+      System.out.println("Bye!");
+    }//eof main()
 }
 
-class CoffeeMachine {
+class Refill implements Coffeeable {
+  private Refill() {}
+  private static final Refill INSTANCE = new Refill();
 
-    int getBrewCapacity() {
-      int wLim = water / waterConsumption;
-      int mLim = milk / milkConsumption;
-      int bLim = beans / beansConsumption;
+  public static final Refill getInstance(int water, int milk, int beans, int cups) {
+    resources.put(ResourceType.WATER, water);
+    resources.put(ResourceType.MILK, milk);
+    resources.put(ResourceType.BEAN, beans);
+    resources.put(ResourceType.CUPS, cups);
+    return INSTANCE;
+  }
 
-      if (wLim <= mLim && wLim <= bLim) {
-        return wLim;
-      } else if (mLim <= wLim && mLim <= bLim) {
-        return mLim;
-      } else {
-        return bLim;
-      }
+  public Map<ResourceType, Integer> getResources(){
+    return resources;
+  }
+
+  private static final Map<ResourceType, Integer> resources = new HashMap<>();
+}
+
+class Drink implements Coffeeable, Payable {
+  public static final Drink ESPRESSO = new Drink(250,0,16,4);
+  public static final Drink LATTE = new Drink(350,75,20,7);
+  public static final Drink CAPPUCHINO = new Drink(200,100,12,6);
+
+  public boolean isCredit(){
+    return true;
+  }
+
+  public int getCashValue(){
+    return cost;
+  }
+
+  private Map<ResourceType, Integer> resources = new HashMap<>();
+  private int cost;
+
+  private Drink(int water, int milk, int beans, int cost) {
+    resources.put(ResourceType.WATER, water);
+    resources.put(ResourceType.MILK, milk);
+    resources.put(ResourceType.BEAN, beans);
+    resources.put(ResourceType.CUPS, 1);
+    this.cost = cost;
+  }
+
+  public Map<ResourceType, Integer> getResources(){
+    return resources;
+  }
+}
+
+
+class VendingMachine implements Coffeeable, Payable {
+
+    public VendingMachine() {
+      resources.put(ResourceType.WATER, 0);
+      resources.put(ResourceType.MILK, 0);
+      resources.put(ResourceType.BEAN, 0);
+      resources.put(ResourceType.CUPS, 0);
     }
 
-    //capacity
-    private final int water;
-    private final int milk;
-    private final int beans;
+    public String getState() {
+      StringBuilder sb = new StringBuilder();
 
-    //per cup consumption
-    private final int waterConsumption;
-    private final int milkConsumption;
-    private final int beansConsumption;
+      sb.append("The coffee machine has:");
+      sb.append(System.getProperty("line.separator"));
+      sb.append(resources.get(ResourceType.WATER) + " of water");
+      sb.append(System.getProperty("line.separator"));
+      sb.append(resources.get(ResourceType.MILK) + " of milk");
+      sb.append(System.getProperty("line.separator"));
+      sb.append(resources.get(ResourceType.BEAN) + " of coffee beans");
+      sb.append(System.getProperty("line.separator"));
+      sb.append(resources.get(ResourceType.CUPS) + " of disposable cups");
+      sb.append(System.getProperty("line.separator"));
+      sb.append(getCashValue() + "$ in cash");
 
-    static class Builder {
-      //capacity
-      private int water = 0;
-      private int milk = 0;
-      private int beans = 0;
-
-      //per cup consumption
-      private final int waterConsumption;
-      private final int milkConsumption;
-      private final int beansConsumption;
-
-      Builder (int waterConsumption, int milkConsumption, int beansConsumption) {
-        this.waterConsumption = waterConsumption;
-        this.milkConsumption = milkConsumption;
-        this.beansConsumption = beansConsumption;
-      }
-
-      Builder water(int val) {
-        water = val;
-        return this;
-      }
-
-      Builder milk(int val) {
-        milk = val;
-        return this;
-      }
-
-      Builder beans(int val) {
-        beans = val;
-        return this;
-      }
-
-      CoffeeMachine build() {
-        return new CoffeeMachine(this);
-      }
+      return sb.toString();
     }
 
-    private CoffeeMachine(Builder builder) {
-      waterConsumption = builder.waterConsumption;
-      milkConsumption = builder.milkConsumption;
-      beansConsumption = builder.beansConsumption;
-      water = builder.water;
-      milk = builder.milk;
-      beans = builder.beans;
+    public int take() {
+      int result = getCashValue();
+      cash = 0;
+      return result;
     }
+
+    public void refill(Coffeeable refill) {
+      if (refill != null) {
+        for (ResourceType resourceType : refill.getResources().keySet()) {
+          int avail = (resources.get(resourceType) == null) ? 0 : resources.get(resourceType);
+          int additional = refill.getResources().get(resourceType);
+          if (additional > 0) {
+              setResource(resourceType, avail+additional);
+          } else {
+            throw new IllegalArgumentException("Refill cannot be negative");
+          }
+        }
+      } else throw new IllegalArgumentException("Cannot refill a null");
+    }
+
+    void setResource(ResourceType type, int val){
+      if (val >= 0) {
+        resources.put(type, val);
+      } else throw new IllegalArgumentException("Resource "+ type +" value cannot be negative, got " + val);
+    }
+
+    public Map<ResourceType, Integer> getResources() {
+      return resources;
+    }
+
+    public boolean isCredit() {
+      return false;
+    }
+
+    public int getCashValue() {
+      return cash;
+    }
+
+    public void order(Drink drink) throws MissingResourceException, IllegalArgumentException {
+      if (drink != null) {
+        for (ResourceType resourceType : drink.getResources().keySet()) {
+          int avail = (resources.get(resourceType) == null) ? 0 : resources.get(resourceType);
+          int required = (drink.getResources().get(resourceType) == null) ? 0 : drink.getResources().get(resourceType);
+          if ((avail-required) < 0) throw new MissingResourceException(resourceType.toString() + " is missing in this machine. Refill required. ", "ResourceType", resourceType.toString());
+          setResource(resourceType, avail-required);
+        }
+        if (drink.getCashValue() >= 0) {
+          cash += drink.getCashValue();
+        } else throw new IllegalArgumentException("Cannot vend a drink with negative cost");
+      } else throw new IllegalArgumentException("Cannot vend a null drink");
+    }
+
+    private Map<ResourceType, Integer> resources = new HashMap<>();
+    private int cash = 0;
+}
+
+interface Coffeeable {
+  enum ResourceType {BEAN, MILK, WATER, CUPS}
+  public Map<ResourceType, Integer> getResources();
+}
+
+interface Payable {
+  /**
+  Determines type of transaction with this item.
+  If true, item costs some value (even if its 0), i.e. Credit Payable.
+  This item can interact only with opposite Payable, i.e. DebitPayable,
+  which should return return false and accumulate values provided with
+  Credit Payables.
+  **/
+  public boolean isCredit();
+
+  public int getCashValue();
 }
